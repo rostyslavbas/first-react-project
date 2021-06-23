1c12e75fbdad35bac221c3547bc30f8ca2b73e9f
rostyslavbas <rostyslavbas@gmail.com>
2021-06-23 14:32:33 +0300

    lesson-1-homework

---
+++ src/users/Users.js
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+import './Users.css'
+export default function Users() {
+
+
+
+   let state = useState([]);
+    let usersList = state[0];
+    let setUsersList = state[1];
+
+    //спрощений синтаксис
+    //let [usersList, setUsersList] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(value => value.json())
+            .then(response => {
+                console.log(response);
+                setUsersList(response);
+            })
+    }, [])
+
+    return (
+        <div>
+            {
+        usersList.map(value => <div className={'comment'}>
+            <h1>{value.name}</h1>
+            <h2>{value.id}</h2>
+            <h2>{value.email}</h2>
+            <p>{value.body}</p>
+            <hr/>
+
+        </div>)
+            }
+        </div>
+    );
+}
